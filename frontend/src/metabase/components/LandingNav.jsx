1c59e33c3ffcb17de29548cf73fdceda1defcb88
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-03-30 15:25:38 -0700

    keep collection context

--- frontend/src/metabase/components/LandingNav.jsx
+++ frontend/src/metabase/components/LandingNav.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Box, Flex } from "rebass";
 import { Link } from "react-router";
 
-import * as Urls from 'metabase/lib/urls'
+import * as Urls from "metabase/lib/urls";
 
 import Icon from "metabase/components/Icon";
 
 const ICON_SIZE = 18;
 
 const LandingNav = props => {
-  const { collectionSlug } = props
+  const { collectionSlug } = props;
   return (
     <Box>
       <Flex>
@@ -19,19 +19,49 @@ const LandingNav = props => {
         </Flex>
         <Flex align="center" mr={1}>
           <Icon name="dashboard" size={ICON_SIZE} className="mr1" />
-          <Link to="dashboards">Dashboards</Link>
+          <Link
+            to={
+              collectionSlug
+                ? `collections/${collectionSlug}/dashboards`
+                : "dashboards"
+            }
+          >
+            Dashboards
+          </Link>
         </Flex>
         <Flex align="center" mr={1}>
           <Icon name="insight" size={ICON_SIZE} className="mr1" />
-          <Link to="metrics">Metrics</Link>
+          <Link
+            to={
+              collectionSlug
+                ? `collections/${collectionSlug}/metrics`
+                : "metrics"
+            }
+          >
+            Metrics
+          </Link>
         </Flex>
         <Flex align="center" mr={1}>
           <Icon name="segment" size={ICON_SIZE} className="mr1" />
-          <Link to="segments">Segments</Link>
+          <Link
+            to={
+              collectionSlug
+                ? `collections/${collectionSlug}/segments`
+                : "segments"
+            }
+          >
+            Segments
+          </Link>
         </Flex>
         <Flex align="center" mr={1}>
           <Icon name="explore" size={ICON_SIZE} className="mr1" />
-          <Link to={ collectionSlug ? Urls.collection({ slug: collectionSlug }) : "questions"}>
+          <Link
+            to={
+              collectionSlug
+                ? Urls.collection({ slug: collectionSlug })
+                : "questions"
+            }
+          >
             Questions
           </Link>
         </Flex>
