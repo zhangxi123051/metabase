d8948d5f09b497d83a65bc89e59c97be9a798949
Ryan Senior <ryan@metabase.com>
2018-04-05 14:27:59 -0500

    Crate test [ci drivers]

--- test/metabase/driver/crate_test.clj
+++ test/metabase/driver/crate_test.clj
@@ -1,7 +1,15 @@
 (ns metabase.driver.crate-test
   (:require [metabase.test.data.datasets :refer [expect-with-engine]]
-            [metabase.test.util :as tu]))
+            [metabase.test.data :as data]
+            [metabase.test.data.dataset-definitions :as defs]
+            [metabase.test.util :as tu]
+            [metabase.sync :as sync]))
 
 (expect-with-engine :crate
   "UTC"
   (tu/db-timezone-id))
+
+(expect-with-engine :crate
+  []
+  (data/with-db (data/get-or-create-database! defs/sad-toucan-incidents)
+    (sync/sync-database! (data/id))))
