1cf86e468eb4338ee57018f0ce6b4e0cb697e7f3
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-06-04 10:39:15 -0700

    add pulse detail

--- frontend/src/metabase/routes.jsx
+++ frontend/src/metabase/routes.jsx
@@ -45,6 +45,7 @@ import ArchiveCollectionModal from "metabase/components/ArchiveCollectionModal";
 import PulseEditApp from "metabase/pulse/containers/PulseEditApp.jsx";
 import PulseListApp from "metabase/pulse/containers/PulseListApp.jsx";
 import PulseMoveModal from "metabase/pulse/components/PulseMoveModal";
+import PulseDetail from "metabase/pulse/containers/PulseDetail"
 import SetupApp from "metabase/setup/containers/SetupApp.jsx";
 import PostSetupApp from "metabase/setup/containers/PostSetupApp.jsx";
 import UserSettingsApp from "metabase/user/containers/UserSettingsApp.jsx";
@@ -316,7 +317,8 @@ export const getRoutes = store => (
       <Route path="/pulse" title={t`Pulses`}>
         <IndexRoute component={PulseListApp} />
         <Route path="create" component={PulseEditApp} />
-        <Route path=":pulseId">
+        <Route path=":pulseId" component={PulseDetail} />
+        <Route path=":pulseId/edit">
           <IndexRoute component={PulseEditApp} />
           <ModalRoute path="move" modal={PulseMoveModal} />
         </Route>
